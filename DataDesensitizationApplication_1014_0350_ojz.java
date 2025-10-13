768a5e0786dcac047b4e8f7cbee1d322cbb5dc16
rEEcEiGnacIo888249 <06.uke-manner@icloud.com>
2025-10-13 21:50:17 +0200

    Configure 数据脱敏工具

---
+++ DataDesensitizationApplication_1014_0350_ojz.java
@@ -0,0 +1,15 @@
+// 代码生成时间: 2025-10-14 03:50:15
+package com.example.datadesensitization;
+
+import org.springframework.boot.SpringApplication;
+# 优化算法效率
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+# FIXME: 处理边界情况
+
+@SpringBootApplication
+public class DataDesensitizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataDesensitizationApplication.class, args);
+    }
+}
