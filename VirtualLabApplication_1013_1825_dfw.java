a1cc4c53e6483f75ca20974a298553152625151b
rEEcEiGnacIo888249 <06.uke-manner@icloud.com>
2025-10-13 12:25:01 +0200

    Configure JAVA 虚拟实验室

---
+++ VirtualLabApplication_1013_1825_dfw.java
@@ -0,0 +1,11 @@
+// 代码生成时间: 2025-10-13 18:25:00
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualLabApplication.class, args);
+    }
+}
